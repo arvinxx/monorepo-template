d1ca6ba70fbd31d915a23b303e845e1ac14d9680
arvinxx <arvinx@foxmail.com>
2021-02-16 16:22:19 +0800

    :wrench: chore(config): 优化配置项

---
+++ packages/bar/.babelrc.js
@@ -0,0 +1,17 @@
+const base = require('../../.babelrc');
+
+module.exports = {
+  ...base,
+  plugins: [
+    ...base.plugins,
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@arvinxu/modules-bar': './src',
+        },
+      },
+    ],
+  ],
+};
